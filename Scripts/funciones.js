a4c1efc12a4484421982388561fc7bfef2e93673
AxelCar20 <axelaaronc@gmail.com>
2022-11-13 21:42:14 -0600

    Primera subida a git

---
+++ Scripts/funciones.js
@@ -0,0 +1,18 @@
+
+/*Para la pantalla del inicio (index.html)*/
+function redirectCatalogo()
+{
+
+window.location.href="./Catalogo/Catalogo.html";
+}
+
+function redirectPedido()
+{
+window.location.href="./Formularios/FormsPedidos.html";
+}
+
+/*Para pantallas secundarias*/
+function redirectPedidoP2()
+{
+window.location.href="../Formularios/FormsPedidos.html";
+}
